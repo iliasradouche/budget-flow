f4f1f776244987c2ff6710ccdeedb9f546fea8dd
Ilias Radouche <iliasradouche@gmail.com>
2026-03-16 17:09:13 +0000

    3rd Commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,57 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['logo.png', 'favicon.svg'],
+      manifest: {
+        name: 'Beztami',
+        short_name: 'Beztami',
+        description: 'Your personal finance tracker',
+        theme_color: '#062616',
+        background_color: '#062616',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'google-fonts-cache', expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 } },
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
+            handler: 'NetworkFirst',
+            options: { cacheName: 'exchange-rates-cache', expiration: { maxEntries: 5, maxAgeSeconds: 60 * 60 } },
+          },
+        ],
+      },
+    }),
+  ],
 })
